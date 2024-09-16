299626e6b03e11e81f6bbbbb099b755eca1459ee
ElenaPetrova56 <ep24659@gmail.com>
2024-09-16 20:17:05 +0300

    Add files via upload

---
+++ modul5.3.py
@@ -0,0 +1,62 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return NotImplemented
+
+    def add(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.add(value)
+
+    def __iadd__(self, value):
+        return self.add(value)
+
+
+house1 = House("Дом 1", 3)
+house2 = House("Дом 2", 3)
+
+print(house1 == house2)
+print(house1 < house2)
+
+house1.add(2)
+print(house1)
